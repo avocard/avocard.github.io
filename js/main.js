29f205484b98286af4a7aacedff113cd3f4c8b94
avocard <yuto.suzuki2006@gmail.com>
2023-02-19 20:44:09 +0900

    [add]少しの改変。僅かによくなったと思う。

--- js/main.js
+++ js/main.js
@@ -1,10 +1,32 @@
+const gameMap = new Map();
+gameMap.set('PokémonHOME','本名' );
+
+//githubにアクセスする関数
 const TwitterImage = document.querySelector('#Twitter');
  TwitterImage.onclick = () => {
   console.log("aaaaaaaaaa")
   window.location.href = "https://twitter.com/avocard_11";
   }  
-  const GithubImage = document.querySelector('#Github');
+
+//twitterにアクセスする関数
+const GithubImage = document.querySelector('#Github');
  GithubImage.onclick = () => {
   console.log("aaaaaaaaaa")
   window.location.href = "https://github.com/avocard";
   }  
+
+  //ゲーム名検索する関数
+  function gameNameChecker(){
+    let element = document.getElementById('gameName');
+console.log(gameMap.has(element));
+      }
+
+  //閲覧にパスワードを追加する関数
+   function gate() {
+    var UserInput = document.getElementById('password');
+    if (UserInput == "aiueo"){
+        gameNameChecker();
+    }
+   }
+
+   
